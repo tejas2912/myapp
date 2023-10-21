fc096dfe523b01401a32bbe7505472499c2b19e8
Your Name <you@example.com>
2023-10-21 13:04:40 +0530

    addition of two number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  name:string='tejas';
+  num1: number=40;
+  num2:number=10;
+  total:number=0;
+  add()
+  {
+    this.total=this.num1+this.num2;
+  }
+  sub()
+  {
+    this.total=this.num1-this.num2;
+  }
+  mul()
+  {
+    this.total=this.num1*this.num2;
+  }
+  div()
+  {
+    this.total=this.num1/this.num2
+  }
 }
